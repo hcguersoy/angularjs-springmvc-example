a05c3c9bbf223bfe3f6095935b909ee563747281
guersoy <halil-cem.guersoy@adesso.de>
2013-08-30 15:55:53 +0200

    added support for tc7.

---
+++ angularjs-springmvc/modules/tc7/manifests/init.pp
@@ -0,0 +1,63 @@
+class tc7 (
+    $http_port = 8844,
+    $https_port = 18443,
+) {
+
+  Package { # defaults
+    ensure => installed,
+  }
+
+  package { 'tomcat7':
+      require => [
+        Package['openjdk-7-jdk'],
+        Package['openjdk-7-jre'],
+        Package['openjdk-7-jre-headless'],
+        Package['openjdk-7-jre-lib'],
+        Package['authbind'],
+        Package['libtcnative-1'],
+      ],
+  }
+
+  package { 'tomcat7-admin':
+      require => Package['tomcat7'],
+  }
+
+  package { 'openjdk-7-jdk':
+  }
+
+  package { 'openjdk-7-jre':
+  }
+
+  package { 'openjdk-7-jre-headless':
+  }
+
+  package { 'openjdk-7-jre-lib':
+  }
+
+  package { 'authbind':
+  }
+
+  package { 'libtcnative-1':
+  }
+
+  file { '/etc/tomcat7/server.xml':
+     owner => 'root',
+     require => Package['tomcat7'],
+     notify => Service['tomcat7'],
+     content => template('tc7/server.xml.erb'),
+  }
+
+  file { '/etc/tomcat7/tomcat-users.xml':
+     owner => 'root',
+     require => Package['tomcat7'],
+     notify => Service['tomcat7'],
+     content => template('tc7/tomcat-users.xml.erb'),
+  }  
+
+  service { 'tomcat7':
+    ensure => running,
+    enable => true,
+    require => Package['tomcat7'],
+  }   
+
+}
