74b9c8509cac328e43a5e8a0904d3c5b72373800
guersoy <halil-cem.guersoy@adesso.de>
2014-03-07 16:18:56 +0100

    Dockerize

---
+++ angularjs-springmvc/docker/build_and_tag.sh
@@ -0,0 +1,20 @@
+#/bin/env bash
+#core provided by Phil Whelan - https://gist.github.com/philwhln/8196116
+
+#build_and_tag <directory of Dockerfile> <resultant docker image name>
+
+DOCKERFILE_DIRECTORY=$1
+DOCKER_IMAGE_NAME=$2
+
+# Build docker image
+rm -f docker-built-id
+docker build --rm=true $DOCKERFILE_DIRECTORY \
+  | perl -pe '/Successfully built (\S+)/ && `echo -n $1 > docker-built-id`'
+if [ ! -f docker-built-id ]; then
+  echo "No docker-built-id file found"
+  exit 1
+fi
+DOCKER_BUILD_ID=`cat docker-built-id`
+
+# Publish built docker image to repo
+docker tag $DOCKER_BUILD_ID $DOCKER_IMAGE_NAME
---
+++ angularjs-springmvc/docker/run_container.sh
@@ -0,0 +1,18 @@
+#/bin/env bash
+#
+# Run a docker image and write the container id (CID) and mapped port into
+# a properties file.
+#
+# run_container.sh <IMAGE_BASENAME> <IMAGE_VERSION> <APP_PORT>
+
+IMAGE_BASENAME=$1
+IMAGE_VERSION=$2
+APP_PORT=$3
+
+rm -f $IMAGE_BASENAME-$IMAGE_VERSION.cid
+docker run -d -p :$APP_PORT --cidfile="$IMAGE_BASENAME-$IMAGE_VERSION.cid" $IMAGE_BASENAME:$IMAGE_VERSION
+CID=`cat $IMAGE_BASENAME-$IMAGE_VERSION.cid`
+PORT=`docker port $CID $APP_PORT | cut -d ':' -f2`
+echo "CID=$CID" > $IMAGE_BASENAME-$IMAGE_VERSION.properties
+echo "PORT=$PORT" >> $IMAGE_BASENAME-$IMAGE_VERSION.properties
+rm $IMAGE_BASENAME-$IMAGE_VERSION.cid
